6ad1f5ea62f54b662d5a46ae052d542f6229a6bb
xudogn <xudogn@qq.com>
2016-11-15 09:51:26 +0800

    Initial Commit

---
+++ BaoChunHui/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  BaoChunHui
+//
+//  Created by xudogn on 16/11/15.
+//  Copyright © 2016年 xudogn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ BaoChunHui/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BaoChunHui
+//
+//  Created by xudogn on 16/11/15.
+//  Copyright © 2016年 xudogn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
